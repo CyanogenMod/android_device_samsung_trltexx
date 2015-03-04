6e87ea3493fd5cb59177732948e1c9a757c3a592
gekkehenkie11 <allard_addink@hotmail.com>
2015-03-04 12:25:48 -0400

    add support 910G
    Change-Id: Id715e6c9c10aca759b316ba88ff9532f52171539

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter trltexx,$(TARGET_DEVICE)),)
+ifneq ($(filter trltedt trltexx,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
